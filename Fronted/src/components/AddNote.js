ded877c0d53ddd06449c04266211669fa35806b1
Jay Kushwaha <jaykush9161@gmail.com>
2024-03-22 21:08:05 +0530

    Updated Fronted

---
+++ Fronted/src/components/AddNote.js
@@ -0,0 +1,27 @@
+//componenets/AddNote.js
+
+import React from "react";
+
+const AddNote = ({ title, setTitle, content, setContent, onAddNote }) => {
+	return (
+		<div className="add-cont">
+			<h2 className="note-h2">Add Note</h2>
+			<input
+				type="text"
+				placeholder="Title"
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+			/>
+			<textarea
+				placeholder="Content"
+				value={content}
+				onChange={(e) => setContent(e.target.value)}
+			></textarea>
+			<button className="button1" onClick={onAddNote}>
+				Add Note
+			</button>
+		</div>
+	);
+};
+
+export default AddNote;
---
+++ Fronted/src/components/Footer.js
@@ -0,0 +1,12 @@
+// footer.jsx
+import React from 'react'
+
+function footer() {
+  return (
+    <div className='footer'>
+        <h2> &copy; 2024 This Notes App website. All rights reserved.</h2>
+        </div>
+  )
+}
+
+export default footer
